b3c7100e57fe6b7c5b9cb8528ee128545220b411
enrique-cabrini <enrique.cabrini@gmail.com>
2022-01-20 09:17:52 +0000

    models file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+mod models;
+
+use crate::models::Status;
+use actix_web::{App, HttpServer, web, Responder};
+use std::io;
+
+async fn status() -> impl Responder {
+    web::HttpResponse::Ok()
+        .json(Status {status : "UP".to_string()})
+}
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    println!("Starting server at 127.0.0.1:5000");
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(status))  
+    })
+        .bind("127.0.0.1:5000")?
+        .run()
+        .await
 }
